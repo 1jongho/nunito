cb9185bce40cc199d9300061e9468a9eb822887e
1jongho <jongho5378@gmail.com>
2025-05-02 23:05:14 +0900

    식물도감 front O, back X, date_picker_modal 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:nunito/firebase_options.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:nunito/screens/homeScreen.dart';
+import 'package:nunito/screens/modaltest.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
